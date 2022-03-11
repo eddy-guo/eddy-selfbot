e98c1af20011c8467215a170f4ead1280bcf4cd0
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-11 13:00:13 -0500

    get media url in data dict

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ BEARER_TOKEN = os.getenv('BEARER_TOKEN')
 headers = {
     'Authorization': f'Bearer {BEARER_TOKEN}',
 }
-data = requests.get(f'https://api.twitter.com/2/users/1453616508035272705/tweets?max_results=5&expansions=attachments.media_keys&media.fields=preview_image_url', headers=headers) # https://tweeterid.com/
+data = requests.get(f'https://api.twitter.com/2/users/1453616508035272705/tweets?max_results=10&expansions=attachments.media_keys&media.fields=preview_image_url,url', headers=headers) # https://tweeterid.com/
 print(data.text)
