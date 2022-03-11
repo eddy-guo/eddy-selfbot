e1fff9a5da1244911923b6b00e72e3f98ac60072
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-11 18:07:45 -0500

    i hate tesseract

--- ocr.py
+++ ocr.py
@@ -1,3 +1,9 @@
 from PIL import Image
 import pytesseract
-import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+im = Image.open("test.jpeg")
+text = pytesseract.image_to_string(im, lang = 'eng')
+
+print(text)
