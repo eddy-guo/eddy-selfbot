bac9ecc28f4b322f9df6f3b976aa1a1aa636cbd3
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-11 16:54:06 -0500

    organize media links

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ headers = {
     'Authorization': f'Bearer {BEARER_TOKEN}',
 }
 data = requests.get(f'https://api.twitter.com/2/users/1453616508035272705/tweets?max_results=5&expansions=attachments.media_keys&media.fields=preview_image_url,url', headers=headers) # https://tweeterid.com/
-media_info = eval(data.content)["includes"]["media"]
 
+# list of tweet media links
+media_info = eval(data.content)["includes"]["media"]
+links = []
 for tweet in range(len(media_info)):
     if "url" in media_info[tweet]:
-        print(media_info[tweet]["url"])
-    elif "preview_image_url" in media_info[tweet]:
-        print(media_info[tweet]["preview_image_url"])
-    
+        links.append(media_info[tweet]["url"])
+    elif "preview_image_url" in media_info[tweet]: # add more elif's if media link 
+        links.append(media_info[tweet]["preview_image_url"])
+print()
+print(links)
