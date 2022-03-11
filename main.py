79acfcab76f6e83f8bf8f229db963512e398c3fa
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-11 05:10:01 -0500

    initial setup

---
+++ main.py
@@ -0,0 +1,27 @@
+import os, discord, pdb
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    global guild
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(f"{bot.user.name} is now connected to {guild.name}.")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    ctx = await bot.get_context(message)
+    await ctx.send("Hi!")
+
+bot.run(TOKEN)
