ebc04203fcd5e1b39fe2408c8e72b81c25fedf63
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-14 05:40:05 -0400

    increase terminal visiblity

--- ocr.py
+++ ocr.py
@@ -6,6 +6,17 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 im = cv2.imread("images/code1.jpg")
 results = pytesseract.image_to_string(im, lang = 'eng')
 
+print("-" * 40)
+print(
+    'Results from tesseract: \n'
+    f'{results}'
+)
+print("-" * 40)
+
 results_list = results.replace(" ", "").split("\n")
 final_list = list(filter(None, results_list))
-print(final_list)
+
+print(
+    'Final list of strings: \n'
+    f'{final_list}')
+print("-" * 40)
