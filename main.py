1d6d80c97a96e577874b82995ed2f3d4a73707ce
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-13 22:54:26 -0400

    import cleantext, fix edgecase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, requests, re
+from cleantext import clean
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -35,32 +36,11 @@ print(
     f'{text}'
 )
 
-# remove spaces, newlines, and emojis from raw text
+# remove spaces and emojis from raw text
 new_text = []
-emoji_patterns = re.compile("["
-                    u"\U0001F600-\U0001F64F"
-                    u"\U0001F300-\U0001F5FF"
-                    u"\U0001F680-\U0001F6FF"
-                    u"\U0001F1E0-\U0001F1FF"
-                    u"\U00002500-\U00002BEF"  
-                    u"\U00002702-\U000027B0"
-                    u"\U00002702-\U000027B0"
-                    u"\U000024C2-\U0001F251"
-                    u"\U0001f926-\U0001f937"
-                    u"\U00010000-\U0010ffff"
-                    u"\u2640-\u2642"
-                    u"\u2600-\u2B55"
-                    u"\u200d"
-                    u"\u23cf"
-                    u"\u23e9"
-                    u"\u231a"
-                    u"\ufe0f"
-                    u"\u3030"
-                    "]+", flags=re.UNICODE)
-
 for x in range(len(text)):
-    temp = emoji_patterns.sub(r'', text[x])
-    new_text.append(temp.replace(" ", ""))
+    temp = clean(text[x], no_emoji=True) # no emojis
+    new_text.append(temp.replace(" ", "")) # no spaces
     
 print('-' * 200)
 print(
