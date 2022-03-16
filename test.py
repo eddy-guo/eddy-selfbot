1d83f7da37db8d64190db6363b7ab837306be5f6
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-16 03:39:22 -0400

    discord invite pypetteer implementation

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-import asyncio, time
+import asyncio
 from pyppeteer import launch
+
+input_selector = '#app-mount > div.app-3xd6d0 > div > div > div > div > form > div > div.block-3uVSn4.marginTop40-Q4o1tS > div.marginBottom8-emkd0_ > div > input'
+submit_selector = '#app-mount > div.app-3xd6d0 > div > div > div > div > form > div > div.block-3uVSn4.marginTop40-Q4o1tS > div:nth-child(3) > button > div'
 
 async def main():
     browser = await launch({'headless': False}, executablePath="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
     page = await browser.newPage()
-    await page.goto('https://google.com')
+    await page.goto('https://discord.gg/F4FjDku3')
+    await page.waitForSelector(input_selector)
 
-    content = "pypetteer"
-    await page.evaluate(f"""() => {{
-        document.getElementsByName('q')[0].value = '{content}';
-    }}""")
+    await page.type(
+        f'{input_selector}',
+        'Eddy Guo'
+        )
+    await page.click(submit_selector)
 
-    # time.sleep(30)
-    await page.screenshot({'path': 'screenshot.png'})
-    await browser.close()
 asyncio.get_event_loop().run_until_complete(main())
