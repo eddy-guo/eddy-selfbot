e8ae0221b8a91882d9f63864b6c25382887d419f
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-16 03:13:27 -0400

    pyppeteer; content input into google search

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-import asyncio
+import asyncio, time
 from pyppeteer import launch
 
 async def main():
-    browser = await launch({'headless': True}, executablePath="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+    browser = await launch({'headless': False}, executablePath="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
     page = await browser.newPage()
     await page.goto('https://google.com')
-    await page.screenshot({'path': 'example.png'})
-    await browser.close()
 
+    content = "pypetteer"
+    await page.evaluate(f"""() => {{
+        document.getElementsByName('q')[0].value = '{content}';
+    }}""")
+
+    # time.sleep(30)
+    await page.screenshot({'path': 'screenshot.png'})
+    await browser.close()
 asyncio.get_event_loop().run_until_complete(main())
