d97a7edc19fd46f26d3c802e5450c31571cebc9b
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-17 03:47:57 -0400

    theoretical logic(?)

--- pyp.py
+++ pyp.py
@@ -3,10 +3,11 @@ from pyppeteer import launch
 from main import text_list
 from ocr import ocr_list
 
+final_list = text_list + ocr_list
 continue_selector = '#app-mount > div.app-3xd6d0 > div > div > div > section > div > button > div'
 login_selector = '#app-mount > div.app-3xd6d0 > div > div > div > div > form > div > div > div.mainLoginContainer-wHmAjP > div.block-3uVSn4.marginTop20-2T8ZJx > button.marginBottom8-emkd0_.button-1cRKG6.button-f2h6uQ.lookFilled-yCfaCM.colorBrand-I6CyqQ.sizeLarge-3mScP9.fullWidth-fJIsjq.grow-2sR_-F > div'
 
-async def main():
+async def main(list):
     browser = await launch({'headless': False,
                             'executablePath': 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe',
                             'args': [
@@ -15,15 +16,19 @@ async def main():
                                        ]
                             })
     page = await browser.newPage()
-    await page.goto('https://discord.gg/F4FjDku3')
-    
-    await page.waitForSelector(continue_selector)
-    await page.click(continue_selector)
-    await page.waitForSelector(login_selector)
-    await page.click(login_selector)
 
-    await browser.close()
+    for item in list:
+        try:
+            await page.goto(f'https://discord.gg/{item}')
+        except:
+            continue
+        else:
+            await page.waitForSelector(continue_selector)
+            await page.click(continue_selector)
+            await page.waitForSelector(login_selector)
+            await page.click(login_selector)
+            await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_until_complete(main(final_list))
 
 # will no longer run; alt account disabled for botting HAHA
