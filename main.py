a740876109f2bf7a404c937722f4add95afc9b13
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-16 20:31:57 -0400

    edgecase; no media in past x tweets

--- main.py
+++ main.py
@@ -11,18 +11,25 @@ headers = {
 data = requests.get(f'https://api.twitter.com/2/users/1453616508035272705/tweets?max_results=5&expansions=attachments.media_keys&media.fields=preview_image_url,url', headers=headers) # https://tweeterid.com/
 
 # list of tweet media links
-media_info = eval(data.content)["includes"]["media"]
-links = []
-for tweet in range(len(media_info)):
-    if "url" in media_info[tweet]:
-        links.append(media_info[tweet]["url"])
-    elif "preview_image_url" in media_info[tweet]: # add more elif's if media link 
-        links.append(media_info[tweet]["preview_image_url"])
-print(
-    '\n'
-    'Media links:\n'
-    f'{links}\n'
-)
+
+def media():
+    if "includes" not in eval(data.content):
+        print("No media links from the tweets scraped.")
+        return
+    else:
+        media_info = eval(data.content)["includes"]["media"]
+        links = []
+        for tweet in range(len(media_info)):
+            if "url" in media_info[tweet]:
+                links.append(media_info[tweet]["url"])
+            elif "preview_image_url" in media_info[tweet]: # add more elif's if media link 
+                links.append(media_info[tweet]["preview_image_url"])
+        print(
+            '\n'
+            'Media links:\n'
+            f'{links}\n'
+        )
+media()
 
 # list of raw text
 text_info = eval(data.content)["data"]
